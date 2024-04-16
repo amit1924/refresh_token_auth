ea52000c342b63fc2b0f8708e52273587bc5394d
amit1924 <amit192400@gmail.com>
2024-04-16 15:57:06 +0530

    client folder updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Registration from "./Registration";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
+// import Home from "./Home";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 
@@ -10,9 +10,10 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Login />}></Route>
+        {/* <Route path="/" element={<Home />}></Route> */}
         <Route path="/register" element={<Registration />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+
         <Route path="/dashboard" element={<Dashboard />}></Route>
       </Routes>
     </BrowserRouter>
